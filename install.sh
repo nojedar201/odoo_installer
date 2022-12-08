4205ff0f0dfbe7febc6a5ca7d6a5afcfa07a9233
Carlos Lopez <celm1990@gmail.com>
2022-12-08 17:25:42 -0500

    IMP: permitir configurar un servidor de postgres externo

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ read -p 'Nombre de Instancia a crear: ' instance_name
 copier copy odoo/ $instance_name
 docker login registry.gitlab.com
 docker-compose -f $instance_name/nginx.yaml up -d
-docker-compose -f $instance_name/postgres.yaml up -d
+if test -f "$instance_name/postgres.yaml"; then
+    docker-compose -f $instance_name/postgres.yaml up -d
+fi
 docker-compose -f $instance_name/odoo.yaml up -d
 sudo venv/bin/python3 $instance_name/task.py --container_name $instance_name
 # detener y recargar para que cojan los cambios en nginx
